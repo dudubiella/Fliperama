15e318e17bca9a8c4111f7b230f29bfe494b0f48
Biella <eduardo.biella@gmail.com>
2024-06-30 11:57:36 -0300

    Inicialização dos jogos e créditos
    Seja quantos forem (dependendo arrumar o front)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, subprocess
 from botao import Button
 import os
 
@@ -15,7 +15,7 @@ def add_fonte(tamanho):
     return pygame.font.Font("imgs/font.ttf", tamanho)
 
 def muda_opcao(atual, acao, max):
-    atual + acao
+    atual += acao
     if atual > max:
         atual = 0
     elif atual < 0:
@@ -29,12 +29,65 @@ def le_creditos(local):
     os.close(fd)
     return content.decode('utf-8')
     
+def exec_jogo(local):
+    subprocess.run(["python", local + "//main.py"])
+    print(local)
+
 def play():
     pygame.display.set_caption("Games")
- 
+
+    opcao = 0
+    
+    botoes = []
+    arquivos = []
+    
+    local_pasta = os.getcwd() + "\\Jogos"
+    lista_jogos = os.listdir(local_pasta)
+
+    for i, nome_arq in enumerate(lista_jogos):
+        nome = os.path.splitext(nome_arq)[0]
+        botoes.append(Button(imagem = None, pos = (240, 250 + 50 * i),
+                             texto = nome, fonte = add_fonte(40), cor = "Black", cor_selecionada = "Green", opcao = i))
+        arquivos.append(local_pasta + "\\" + nome_arq)
+
+    play_sair = Button(imagem = pygame.image.load("imgs/Quit Rect.png"), pos = (1050, 600),
+                           texto = "SAIR", fonte = add_fonte(75), cor = "Black", cor_selecionada = "Green", opcao = i + 1)
+
     while True:
         TELA.blit(BG, (0, 0))
+
+        texto_jogos = add_fonte(75).render("JOGOS", True, "#6699CC")
+        jogos_certo = texto_jogos.get_rect(center = (240, 100))
+        
+        TELA.blit(texto_jogos, jogos_certo)
+
+        play_sair.muda_cor(opcao)
+        play_sair.atualiza(TELA)
+        
+        for botao in botoes:
+            botao.muda_cor(opcao)
+            botao.atualiza(TELA)
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_UP: 
+                    opcao = muda_opcao(opcao, -1, 2)
+                elif evento.key == pygame.K_DOWN:
+                    opcao = muda_opcao(opcao, 1, 2)
+
+                elif evento.key == pygame.K_SPACE:
+                    for botao in botoes:
+                        if botao.verifica_entrada(opcao):
+                            exec_jogo(arquivos[botao.opcao])
+                    if play_sair.verifica_entrada(opcao):
+                        main_menu()
+        
         pygame.display.update()
+
 
 def creditos():
     pygame.display.set_caption("Creditos")
@@ -44,24 +97,27 @@ def creditos():
     botoes = []
     conteudos = []
     
-    local_pasta = os.getcwd()
-    lista_colaboradores = os.listdir(local_pasta + "\\creditos")
+    local_pasta = os.getcwd() + "\\creditos"
+    lista_colaboradores = os.listdir(local_pasta)
 
     for i, nome_arq in enumerate(lista_colaboradores):
-        local = local_pasta + "\\creditos" + "\\" + nome_arq
-        conteudos.append(le_creditos(local))
+        conteudos.append(le_creditos(local_pasta + "\\" + nome_arq))
         nome = os.path.splitext(nome_arq)[0]
-        botoes.append(Button(imagem = pygame.image.load("imgs/Options Rect.png"), pos = (640, 200 + 130 * i),
+        botoes.append(Button(imagem = None, pos = (640, 250 + 130 * i),
                              texto = nome, fonte = add_fonte(70), cor = "Black", cor_selecionada = "Green", opcao = i))
 
-    creditos_sair = Button(imagem = pygame.image.load("imgs/Quit Rect.png"), pos = (640, 460),
+    creditos_sair = Button(imagem = pygame.image.load("imgs/Quit Rect.png"), pos = (640, 600),
                            texto = "SAIR", fonte = add_fonte(75), cor = "Black", cor_selecionada = "Green", opcao = i + 1)
     
     while True:
         TELA.fill("white")
 
-        creditos_sair.muda_cor(opcao)
+        texto_creditos = add_fonte(75).render("CREDITOS", True, "#6699CC")
+        creditos_certo = texto_creditos.get_rect(center = (640, 100))
+        
+        TELA.blit(texto_creditos, creditos_certo)
 
+        creditos_sair.muda_cor(opcao)
         creditos_sair.atualiza(TELA)
         
         for botao in botoes:
@@ -74,16 +130,10 @@ def creditos():
                 sys.exit()
 
             if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_UP:
-                    if opcao <= 0:
-                        opcao = 2
-                    else:
-                        opcao -= 1
+                if evento.key == pygame.K_UP: 
+                    opcao = muda_opcao(opcao, -1, i + 1)
                 elif evento.key == pygame.K_DOWN:
-                    if opcao >= 2:
-                        opcao = 0
-                    else:
-                        opcao += 1
+                    opcao = muda_opcao(opcao, 1, i + 1)
 
                 elif evento.key == pygame.K_SPACE:
                     ##se for realizar algo ao selecionar alguem nos créditos
@@ -102,19 +152,19 @@ def main_menu():
     
     opcao = 0
     
+    but_play = Button(imagem = pygame.image.load("imgs/Play Rect.png"), pos = (640,250),
+                        texto = "PLAY", fonte = add_fonte(75), cor = "#d7fcd4", cor_selecionada = "white", opcao = 0)
+    but_creditos = Button(imagem = pygame.image.load("imgs/Options Rect.png"), pos = (640,400),
+                        texto = "CREDITS", fonte = add_fonte(75), cor = "#d7fcd4", cor_selecionada = "white", opcao = 1)
+    but_sair = Button(imagem = pygame.image.load("imgs/Quit Rect.png"), pos = (640,550),
+                        texto = "SAIR", fonte = add_fonte(75), cor = "#d7fcd4", cor_selecionada = "white", opcao = 2)
+    
     while True:
         TELA.blit(BG, (0, 0))
 
         texto_menu = add_fonte(100).render("MENU", True, "#b68f40")
         menu_certo = texto_menu.get_rect(center = (640, 100))
         
-        but_play = Button(imagem = pygame.image.load("imgs/Play Rect.png"), pos = (640,250),
-                          texto = "PLAY", fonte = add_fonte(75), cor = "#d7fcd4", cor_selecionada = "white", opcao = 0)
-        but_creditos = Button(imagem = pygame.image.load("imgs/Options Rect.png"), pos = (640,400),
-                          texto = "CREDITS", fonte = add_fonte(75), cor = "#d7fcd4", cor_selecionada = "white", opcao = 1)
-        but_sair = Button(imagem = pygame.image.load("imgs/Quit Rect.png"), pos = (640,550),
-                          texto = "SAIR", fonte = add_fonte(75), cor = "#d7fcd4", cor_selecionada = "white", opcao = 2)
-
         TELA.blit(texto_menu, menu_certo)
 
         for button in [but_play, but_creditos, but_sair]:
@@ -127,17 +177,11 @@ def main_menu():
                 sys.exit()
 
             if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_UP:
-                    if opcao <= 0:
-                        opcao = 2
-                    else:
-                        opcao -= 1
+                if evento.key == pygame.K_UP: 
+                    opcao = muda_opcao(opcao, -1, 2)
                 elif evento.key == pygame.K_DOWN:
-                    if opcao >= 2:
-                        opcao = 0
-                    else:
-                        opcao += 1
-
+                    opcao = muda_opcao(opcao, 1, 2)
+                
                 elif evento.key == pygame.K_SPACE:
                     if but_play.verifica_entrada(opcao):
                         play()
